9c481dd17735c9847ea2d80d3dc888ab19175b2d
Wenlin Fan <67357819+elainekiwi@users.noreply.github.com>
2024-12-12 14:11:03 +0800

    Update process.py

--- datasets/process.py
+++ datasets/process.py
@@ -7,13 +7,17 @@ def csv2tensor(file_path):
     #output: a dictionary of tensor converted from csv file, representing different sites for EEG
     df = pd.read_csv(file_path)
     tensor_columns = {col: torch.tensor(df[col].values, dtype=torch.float32) for col in df.columns}
-    return tensor_columns
+    tensor_list=list(tensor_columns.values())
+    # for i in tensor_list:
+    #     print(i.shape)
+    res=torch.stack(tensor_list, dim=0)
+    return res
 
 folder_path = os.getcwd()
 files = [f for f in os.listdir(folder_path) if f.endswith('.csv')]
 
-for file_name in files:
-    file_path = os.path.join(folder_path, file_name)
-    tensor_columns = csv2tensor(file_path)
-    #test
-    print(tensor_columns)
+#test
+# for file_name in files:
+#     file_path = os.path.join(folder_path, file_name)
+#     tensor = csv2tensor(file_path)
+#     print(tensor.shape)
